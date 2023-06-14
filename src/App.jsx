ce5bd804ba24db5fde6bc05bd1b8b3d0ac0549a4
Lakshya1711 <Lucky17sh@gmail.com>
2023-06-14 22:29:41 +0530

    Install sass and created board

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import './App.css';
-import Square from './components/square';
+import './styles.scss';
+import Board from './components/Board';
 
 function App() {
   return (
-    <div className="card">
-      <h1>Tic Tac Toe</h1>
-      <img src="/vite.svg" alt="" />
-
-      <Square value="Lakshya">
-        <div>Heelo there</div>
-        <h5>Title</h5>
-      </Square>
-      <Square value="Srivastava" />
-      <Square value="5" />
+    <div className="app">
+      <Board />
     </div>
   );
 }
--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,12 +1,8 @@
-const Square = ({ value, children }) => {
-  console.log(children);
+const Square = ({ value }) => {
   return (
-    <div>
+    <button type="button" className="square">
       {value}
-
-      <h5>Children will be rendered below</h5>
-      <div>{children}</div>
-    </div>
+    </button>
   );
 };
 
