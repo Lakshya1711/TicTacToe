d142a181e51840045aad4419ab8985ba872a0823
Lakshya1711 <Lucky17sh@gmail.com>
2023-06-15 23:32:41 +0530

    Insert 'X' on Click

--- Readme.md
+++ Readme.md
@@ -1,1 +1,13 @@
 # Tic_Tac_Toe Game
+
+git add . (to Staged the files)
+
+git commit -m "" (commit the files)
+
+npm run dev (to run the project)
+
+Dependency to Download
+
+1. Sass
+2. Scss
+3. Vite
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import './styles.scss';
 import Board from './components/Board';
 
 function App() {
+  const [counter, setcounter] = useState(1);
+
+  const onBtnclick = () => {
+    setcounter(currentcounter => {
+      return currentcounter + 1;
+    });
+  };
   return (
     <div className="app">
-      <Board />
+      <div>
+        <button onClick={onBtnclick}></button>
+        {/* <div>{counter}</div> */}
+        <Board />
+      </div>
     </div>
   );
 }
